8cde9600cc55dcb6fd15c73c8343883f86f569bc
Eduardo Zuniga <lord_zeg25@hotmail.com>
2023-05-01 16:44:56 -0500

    include patrons

--- models/policy.js
+++ models/policy.js
@@ -1,26 +1,16 @@
 const constant = require('../config/constant');
 
-const Policy = {
+class Policy {
 
-    id: {
-        type: String
-    },
-    amountInsured: {
-        type: Number,
-    },
-    email: {
-        type: String,
-        unique: true
-    },
-    inceptionDate: {
-        type: String
-    },
-    installmentPayment: {
-        type: Boolean
-    },
-    clientId: {
-        type: String
+    constructor(id, amountInsured, email, inceptionDate, installmentPayment, clientId) {
+        this.id = id;
+        this.amountInsured = amountInsured;
+        this.email = email;
+        this.inceptionDate = inceptionDate;
+        this.installmentPayment = installmentPayment;
+        this.clientId = clientId;
     }
+
 };
 
 module.exports = Policy;
